e322fbed21e5b423d6b3d1fdc4230db2baad3499
amirparvaneh <amirparvaneh138@gmail.com>
2023-09-15 19:46:25 +0330

    challenge#1

--- fundamental-1/script.js
+++ fundamental-1/script.js
@@ -19,3 +19,20 @@ let johnBmi = bmi(John.weight,John.height);
 let markHigherBmi = markBmi > johnBmi;
 
 console.log(markBmi,johnBmi,markHigherBmi);
+
+
+let statusResult = "Mark's BMI is higher than John's!"
+
+if (markHigherBmi){
+    console.log(statusResult);
+}else{
+    statusResult = "John's BMI is higher than Mark's!"
+}
+
+let statusWithTemplate = `Mark's BMI is ${markBmi} and is higher than john with BMI ${johnBmi}`;
+
+if(markHigherBmi){
+    console.log(statusWithTemplate)
+}else{
+    statusWithTemplate = `John's BMI is ${johnBmi} and is higher than mark with BMI ${markBmi}`;
+}
