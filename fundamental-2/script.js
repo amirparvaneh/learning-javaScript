f200f2aed9ed44c34cbd4efd703988943c1bb8ae
amirparvaneh <amirparvaneh138@gmail.com>
2023-09-22 11:32:00 +0330

    challenge#1

---
+++ fundamental-2/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const calculateAvg = (t1,t2,t3) => {
+    let result = (t1 + t2 + t3)/3;
+    return result;
+}
+
+let avgKoalas = calculateAvg(65,54,49);
+let avgDolphins = calculateAvg(4444,23,71);
+
+let gameStatus = "";
+let winner; 
+const checkWinner = (x,y) => {
+    x >= y*2 ? winner = "Koalas" : ((y > x*2) ? winner = "Dolphins" : null); 
+    return winner;
+}
+
+let result = checkWinner(avgKoalas,avgDolphins);
+
+if(result === "Koalas"){
+    gameStatus =  `${winner} win ${avgKoalas}vs${avgDolphins}`;
+}else if(result === "Dolphins"){
+    gameStatus = `${winner} win ${avgDolphins}vs${avgKoalas}`;
+}else {
+    console.log("no winner");
+}
+
+console.log(gameStatus);
