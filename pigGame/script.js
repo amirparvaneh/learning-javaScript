294325baa61154ab20b4236db65c2b137a91a27a
amirparvaneh <amirparvaneh138@gmail.com>
2023-10-14 23:43:01 +0330

    pigGame

--- pigGame/script.js
+++ pigGame/script.js
@@ -1,4 +1,10 @@
 'use-script';
 'use-strict';
 
-const score0El = document.querySelector(#score--0)
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add('hidden');
